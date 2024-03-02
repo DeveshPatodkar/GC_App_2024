8912b5492b117669762e276d69ef2a8b1ec0ee9f
ayush1108g <105841420+ayush1108g@users.noreply.github.com>
2024-03-03 01:40:23 +0530

    #LeeaderBoard and Event screen error fixed

--- App.js
+++ App.js
@@ -1,44 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
-import { useState } from 'react';
-import OngoingScreen from './screens/OngoingScreen';
-import UpcomingScreen from './screens/UpcomingScreen';
-import TopMostCard from './components/TopMostCard';
-import OngoingUpcomingButton from './components/OngoingUpcomingButtons';
-import Leaderboard from "./screens/Leaderboard";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { useState } from "react";
+import OngoingScreen from "./screens/OngoingScreen";
+import UpcomingScreen from "./screens/UpcomingScreen";
+import TopMostCard from "./components/TopMostCard";
+import OngoingUpcomingButton from "./components/OngoingUpcomingButtons";
+
+//LeaderBoard Screen
+// import Leaderboard from "./screens/Leaderboard";
+
 export default function App() {
+  const [screen, setScreen] = useState(1);
+  function renderOngoing() {
+    setScreen(1);
+  }
+  function renderUpcoming() {
+    setScreen(0);
+  }
 
-const [screen, setScreen] = useState(1);
-function renderOngoing() {
-  setScreen(1);
+  return (
+    <SafeAreaView style={styles.container}>
+      <TopMostCard />
+      <View style={styles.ButtonContainer}>
+        <OngoingUpcomingButton
+          onPress={renderOngoing}
+          currentScreen={screen}
+          currentButton={0}
+        >
+          ONGOING
+        </OngoingUpcomingButton>
+        <OngoingUpcomingButton
+          onPress={renderUpcoming}
+          currentScreen={screen}
+          currentButton={1}
+        >
+          UPCOMING
+        </OngoingUpcomingButton>
+      </View>
+      {screen == 1 ? <OngoingScreen /> : <UpcomingScreen />}
+    </SafeAreaView>
+  );
 }
-function renderUpcoming() {
-  setScreen(0);
-}
-
-return (
-      <SafeAreaView style={styles.container}>
-        <TopMostCard/>
-        <View style={styles.ButtonContainer}>
-            <OngoingUpcomingButton onPress={renderOngoing} currentScreen={screen} currentButton={0}>ONGOING</OngoingUpcomingButton>
-            <OngoingUpcomingButton onPress={renderUpcoming} currentScreen={screen} currentButton={1}>UPCOMING</OngoingUpcomingButton>
-        </View>
-        {screen == 1 ? (
-          <OngoingScreen/>
-        ) : (
-          <UpcomingScreen/>
-        )}
-      </SafeAreaView>
-
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow:1,
-    backgroundColor: '#000000',
+    flexGrow: 1,
+    backgroundColor: "#000000",
   },
   ButtonContainer: {
-        marginLeft: 16,
-        flexDirection: 'row',
-        marginBottom: 24
-    },
+    marginLeft: 16,
+    flexDirection: "row",
+    marginBottom: 24,
+  },
 });
